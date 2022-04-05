00265ecb40d552e125999a05d98fe6ab3eb2352d
João Lee <joaumlimaum@gmail.com>
2022-04-05 18:06:31 -0300

    Requisitos 19 e 20

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const characterCounter = document.getElementById('counter');
+const inputTextArea = document.getElementById('textarea');
 const btn = document.querySelector('#botao');
+// const submissionButton = document.getElementsById('submit-btn');
+// const formInfo = document.getElementById('evaluation-form')
 
 function login() {
   const email = document.querySelector('#email');
@@ -11,3 +15,23 @@ function login() {
   }
 }
 btn.addEventListener('click', login);
+const submitButton = document.querySelector('#submit-btn')
+const agreementButton = document.querySelector('#agreement');
+
+function buttonAppearance() {
+  if (agreementButton.checked) {
+    submitButton.removeAttribute('disabled');
+  }
+}
+agreementButton.addEventListener('click', buttonAppearance);
+
+function counter() {
+  let initialCount = characterCounter.innerText;
+  initialCount = 500;
+  characterCounter.innerText = initialCount - inputTextArea.value.length;
+}
+inputTextArea.addEventListener('keyup', counter);
+
+formInfo.addEventListener('submit', function(event){
+  event.preventDefault()
+})
